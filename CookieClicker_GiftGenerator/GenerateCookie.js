8852acf8b1a00b0fb7c091dbcda6e4ff55775f83
Evanski <61751485+EvanSkiStudios@users.noreply.github.com>
2023-05-31 13:53:07 -0500

    Update GenerateCookie.js

--- CookieClicker_GiftGenerator/GenerateCookie.js
+++ CookieClicker_GiftGenerator/GenerateCookie.js
@@ -4,7 +4,7 @@ function GenerateGiftCode(){
   //var giftBoxDesign_Y = round(random(0,37));
   //"34 "+round(random(0,7)); - normal gift icons
   
-  var giftString = ("MAIL|" + Date.now() + "|1000|10 6|Generated Cookies taste stale|");
+  var giftString = ("MAIL|" + Date.now() + "|1000|10 6|Generated Cookies\ntaste stale.|");
   return btoa(giftString); 
 }
 
